d5b159b7c0e2da7c5e35b2f7f0e92ad3171ec4b2
hectorproko <38566819+hectorproko@users.noreply.github.com>
2025-08-23 20:48:20 -0400

    create db

--- python/python_api/main.py
+++ python/python_api/main.py
@@ -5,6 +5,29 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__) # as in name of the flask app is going to be app
 
 # Create Databse
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travel.db"
+
+db = SQLAlchemy(app)
+
+#building a model
+class Destination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    
+    #he wants to turn the above into json
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "destination" : self.destination,
+            "country" : self.country,
+            "rating" : self.rating
+        }
+
+# create context manager
+with app.app_context():
+    db.create_all()
 
 
 # Create Routes
